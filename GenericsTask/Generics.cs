56ac26cdc5a365c49d1a98b00167c3145410c9a3
Oleh Hrytsenko <ohrytsen@softserveinc.com>
2017-12-18 13:18:28 +0200

    Generics task

---
+++ GenericsTask/Generics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsTask
+{
+    class Generics
+    {
+        static void Main(string[] args)
+        {
+            List<String> values = new List<String>(){ "5solar", "4solar", "NY", "3solar", "error" };
+            List<String> searchResults = values.Where(s => s.Contains("solar")).ToList();
+
+            searchResults = searchResults.Select(s => { s += "_winds";return s;}).ToList();
+
+            searchResults.ForEach(Console.WriteLine);
+            
+            Console.ReadLine();
+        }
+    }
+}
